5cb64432fb9c8da24c645440d454d045188981ec
hankberger <hanksberger@gmail.com>
2022-09-27 15:16:24 -0500

    base

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,100 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { CharacterControls } from './characterControls';
+import * as THREE from 'three'
+import { CameraHelper } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 5;
+camera.position.x = 0;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true
+
+// CONTROLS
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 15
+orbitControls.enablePan = false
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05
+orbitControls.update();
+
+// LIGHTS
+light()
+
+// FLOOR
+generateFloor()
+
+// MODEL WITH ANIMATIONS
+new GLTFLoader().load('soldier.glb', function (gltf) {
+    const model = gltf.scene;
+    model.traverse(function (object: any) {
+        if (object.isMesh) object.castShadow = true;
+    });
+    scene.add(model);
+
+    const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
+    const mixer = new THREE.AnimationMixer(model);
+    const animationsMap: Map<string, THREE.AnimationAction> = new Map()
+    gltfAnimations.filter(a => a.name != 'TPose').forEach((a: THREE.AnimationClip) => {
+        animationsMap.set(a.name, mixer.clipAction(a))
+    })
+});
+
+const clock = new THREE.Clock();
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
+
+function generateFloor() {
+    // TEXTURE
+
+    const WIDTH = 80
+    const LENGTH = 80
+
+    const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+    const material = new THREE.MeshBasicMaterial(
+        {
+           
+        })
+   
+    // const material = new THREE.MeshPhongMaterial({ map: placeholder})
+
+    const floor = new THREE.Mesh(geometry, material)
+    floor.receiveShadow = true
+    floor.rotation.x = - Math.PI / 2
+    scene.add(floor)
+}
+
+function light() {
+    scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(- 60, 100, - 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    scene.add(dirLight);
+    // scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+}
